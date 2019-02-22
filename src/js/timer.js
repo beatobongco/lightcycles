0ded6f93b5b82f6273a7f4db2624753e3a1e1eb0
Beato Bongco <beatobongco@gmail.com>
2019-02-22 09:38:30 +0800

    Fix timer

--- src/js/timer.js
+++ src/js/timer.js
@@ -1,28 +1,40 @@
 import { playTick } from './sound';
 
 let start = null;
-let roundTime = 30;
 
-const createTimer = _ => {
-  G.timeLeft = roundTime;
-  document.getElementById('timer').innerText = roundTime;
+const createTimer = () => {
+  clearInterval(G.gameTimer);
+  G.timeLeft = G.roundTime;
+  document.getElementById('timer').innerText = G.roundTime;
   start = Date.now();
   return setInterval(function() {
     const delta = Date.now() - start;
     const timer = document.getElementById('timer');
-    G.timeLeft = roundTime - Math.floor(delta / 1000);
+    G.timeLeft = G.roundTime - Math.floor(delta / 1000);
     timer.innerText = G.timeLeft;
     if (G.timeLeft <= 0) {
       G.gameOver = true;
       clearInterval(G.gameTimer);
     }
+    const tc = timer.classList;
     if (G.timeLeft <= 5) {
-      if (!timer.classList.contains('time-low')) {
-        timer.classList.add('time-low');
+      if (!tc.contains('time-low')) {
+        tc.add('time-low');
       }
       playTick();
+    } else if (tc.contains('time-low')) {
+      tc.remove('time-low');
     }
   }, 1000);
 };
 
-export { createTimer };
+function addTime(s) {
+  start += s * 1000;
+}
+
+function setTime(s) {
+  start = Date.now();
+  G.timeLeft = s;
+}
+
+export { createTimer, addTime, setTime };
